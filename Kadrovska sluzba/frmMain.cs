bc6d44fe3214db783c8835daf4b0cf37c113a495
sindab <sindab@gmail.com>
2017-09-17 14:23:50 +0200

    Add project files.

---
+++ Kadrovska sluzba/frmMain.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+
+namespace Kadrovska_sluzba
+{
+    public partial class frmMain : DevExpress.XtraBars.Ribbon.RibbonForm
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+        void navBarControl_ActiveGroupChanged(object sender, DevExpress.XtraNavBar.NavBarGroupEventArgs e)
+        {
+            SetujRadnika();
+            navigationFrame.SelectedPageIndex = navBarControl.Groups.IndexOf(e.Group);
+        }
+        void barButtonNavigation_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            SetujRadnika();
+            int barItemIndex = barSubItemNavigation.ItemLinks.IndexOf(e.Link);
+            navBarControl.ActiveGroup = navBarControl.Groups[barItemIndex];
+        }
+
+        private void frmMain_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void barButtonItem1_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            SetujRadnika();
+            navBarControl.ActiveGroup= navBarControl.Groups[0];
+        }
+
+        private void barButtonItem1_ItemClick_1(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            SetujRadnika();
+            navBarControl.ActiveGroup = navBarControl.Groups[1];
+        }
+
+        //private void ucRadnici1_OnPromjenaRadnika(object myObject, ucRadnici.RadnikArgs myArgs)
+        //{
+        //    ucRadnik1.Radnik = ucRadnici1.TrenutniRadnik;
+        //    //navBarControl.ActiveGroup = navBarControl.Groups[1];
+        //}
+        private void ucRadnici1_IzmjenaRadnika(object myObject, ucRadnici.RadnikArgs myArgs)
+        {
+            SetujRadnika();
+            navBarControl.ActiveGroup = navBarControl.Groups[1];
+        }
+        private void OtvoriJednogRadnika()
+        {
+            ucRadnik1.Radnik = ucRadnici1.TrenutniRadnik;
+        }
+    }
+}
---
+++ Kadrovska sluzba/ucRadnici.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraBars;
+using DevExpress.XtraEditors;
+using System.ComponentModel.DataAnnotations;
+using Kadrovska_sluzba.DB.Models;
+using Kadrovska_sluzba.DB.Service;
+
+namespace Kadrovska_sluzba
+{
+    public partial class ucRadnici : DevExpress.XtraEditors.XtraUserControl
+    {
+        IEnumerable<Radnik> dataSource;
+        Radnik trenutni;
+        public delegate void RadnikChangedHandler(object myObject, RadnikArgs myArgs);
+        //public event RadnikChangedHandler OnPromjenaRadnika;
+        public event RadnikChangedHandler IzmjenaRadnika;
+
+        public ucRadnici()
+        {
+            InitializeComponent();
+        }
+
+        private void ucRadnici_Load(object sender, EventArgs e)
+        {
+            //dataSource = GetDataSource();
+            //gridControl.DataSource = dataSource;
+            //bsiRecordsCount.Caption = "RECORDS : " + dataSource.ToList().Count;
+        }
+
+        public Radnik TrenutniRadnik
+        {
+            get
+            {
+                trenutni = dataSource.First(x => x.ID == (int)gridView.GetFocusedRowCellValue("ID"));
+                if (trenutni is null)
+                {
+                    trenutni = new Radnik();
+                }
+                return trenutni;
+            }
+        }
+
+        public class RadnikArgs : EventArgs
+        {
+            private Radnik radnik;
+            public RadnikArgs(Radnik radnik)
+            {
+                this.radnik = radnik;
+            }
+            // This is a straightforward implementation for declaring a public field
+            public Radnik Radnik
+            {
+                get
+                {
+                    return radnik;
+                }
+            }
+        }
+        void bbiPrintPreview_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            gridControl.ShowRibbonPrintPreview();
+        }
+        public IEnumerable<Radnik> GetDataSource()
+        {
+            RadnikService rs = new RadnikService();
+            IEnumerable<Radnik> result = rs.GetAll();
+            //BindingList<Customer> result = new BindingList<Customer>();
+            //result.Add(new Customer()
+            //{
+            //    ID = 1,
+            //    Name = "ACME",
+            //    Address = "2525 E El Segundo Blvd",
+            //    City = "El Segundo",
+            //    State = "CA",
+            //    ZipCode = "90245",
+            //    Phone = "(310) 536-0611"
+            //});
+            //result.Add(new Customer()
+            //{
+            //    ID = 2,
+            //    Name = "Electronics Depot",
+            //    Address = "2455 Paces Ferry Road NW",
+            //    City = "Atlanta",
+            //    State = "GA",
+            //    ZipCode = "30339",
+            //    Phone = "(800) 595-3232"
+            //});
+            return result;
+        }
+        //public class Customer
+        //{
+        //    [Key, Display(AutoGenerateField = false)]
+        //    public int ID { get; set; }
+        //    [Required]
+        //    public string Name { get; set; }
+        //    public string Address { get; set; }
+        //    public string City { get; set; }
+        //    public string State { get; set; }
+        //    [Display(Name = "Zip Code")]
+        //    public string ZipCode { get; set; }
+        //    public string Phone { get; set; }
+        //}
+
+        private void bbiEdit_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            //Customer c = dataSource.First(x => x.ID == (int)gridView.GetFocusedRowCellValue("ID"));
+            RadnikArgs myArgs = new RadnikArgs(TrenutniRadnik);
+            IzmjenaRadnika(this, myArgs);
+            //raise event RadnikPromjenjen(radnik)
+            //frmRadnikEdit fRE = new frmRadnikEdit();
+            //fRE.ShowDialog();
+        }
+
+        private void gridView_FocusedRowChanged(object sender, DevExpress.XtraGrid.Views.Base.FocusedRowChangedEventArgs e)
+        {
+            //RadnikArgs myArgs = new RadnikArgs(TrenutniRadnik);
+            //OnPromjenaRadnika(this, myArgs);
+        }
+
+    }
+}
